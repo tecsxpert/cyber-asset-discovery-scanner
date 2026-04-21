d940c2959f2b49e49f288cd63352bfa613d16f7f
shreedhar <shreedharls032@gmail.com>
2026-04-21 18:00:38 +0530

    Day 1- created flyway v1 migration for assets table, Day 2-completed frontend setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-gray-900 text-white text-3xl">
+      Cyber Scanner Day 2 Setup Done 🚀
+    </div>
+  )
+}
+
+export default App
