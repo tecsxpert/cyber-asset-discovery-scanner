b62400569a23381875256e8a31a570c2d850b9ce
HarismithaSN <harismithasn@gmail.com>
2026-04-29 11:23:04 +0530

    Save local changes before merging main

--- backend/src/test/java/com/internship/tool/service/CyberAssetServiceImplTest.java
+++ backend/src/test/java/com/internship/tool/service/CyberAssetServiceImplTest.java
@@ -24,8 +24,6 @@ import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyLong;
-import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.*;
 
 /**
